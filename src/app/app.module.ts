a024d379478d6c6d8f40afff36257aeee3f8e3f2
unknown <31121986g@gmail.com>
2019-06-04 10:31:08 +0530

    revise learning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, WarningComponent
   ],
   imports: [
     BrowserModule
